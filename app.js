9c9ca78717a7a1bf3f304458b5ea82857490903e
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-12 14:05:03 -0700

    setup babel/webpack and start building bundle.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const koa = require('koa');
+const koaMount = require('koa-mount');
+const koaRouter = require('koa-router');
+const koaStatic = require('koa-static');
 const koaViews = require('koa-views');
-
 
 const app = new koa();
 
+// Pre-routes middleware!
 app.use(koaViews(__dirname + '/views'));
+app.use(koaMount('/static', koaStatic('static')));
 
-app.use(async ctx => {
+// Routes
+const router = new koaRouter();
+
+router.get('/', async ctx => {
   await ctx.render('index.html.pug');
 });
 
+app.use(router.routes());
+
+// Listen!
 app.listen(3000);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './frontend/app.js',
+  ],
+
+  // The fuck is the point of nesting this under "module"?
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+  },
+};
