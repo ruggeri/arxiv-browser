0be31101f1bc2850db5acb683ae686f5ad47f000
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-12 14:10:25 -0700

    begin the great rendering of React!

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hello world from React!</h1>, root);
+});
