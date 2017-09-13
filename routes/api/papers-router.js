8dcfb0b62c7ae4f2d40d3100d8891f62286fc57d
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-12 20:57:06 -0700

    serve ALL the papers in JSON

--- routes/api/papers-router.js
+++ routes/api/papers-router.js
@@ -2,10 +2,15 @@ const koaRouter = require('koa-router');
 
 const papersRouter = new koaRouter();
 papersRouter.get('/', async ctx => {
-  const paper = await ctx.models.Paper.findOne()
-  ctx.body = {
-    [paper.id]: paper
-  };
+  // TODO: eventually I will want to paginate this...
+  const papers = await ctx.models.Paper.findAll(
+    {order: [['publicationDateTime', 'DESC']]}
+  );
+
+  ctx.body = {}
+  for (paper of papers) {
+    ctx.body[paper.link] = paper.toJSON();
+  }
 });
 
 module.exports = papersRouter;
