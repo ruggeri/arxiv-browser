7c4d15bb56b3114b7ce758684085cd1097fb209f
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-13 13:45:49 -0700

    move AuthorsShow where it belongs!

--- frontend/components/app-root.jsx
+++ frontend/components/app-root.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PapersIndex from './papers/papers-index.jsx';
 import PaperShow from './papers/paper-show.jsx';
-import AuthorShow from './papers/author-show.jsx';
+import AuthorShow from './authors/author-show.jsx';
 
 const AppRoot = ({ store }) => (
   <Provider store={store}>
