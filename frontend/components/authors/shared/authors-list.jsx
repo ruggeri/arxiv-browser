41292a19fef87d8518cbbbf1c990fa949aa8e9ee
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-14 01:15:34 -0700

    don't dump all author elements into the DOM simultaneously

--- frontend/components/authors/shared/authors-list.jsx
+++ frontend/components/authors/shared/authors-list.jsx
@@ -1,10 +1,32 @@
 import classNames from 'classnames';
 import AuthorItem from 'components/authors/shared/author-item.jsx';
+import ResultsPager from 'helpers/results-pager';
+import { List } from 'immutable';
 import React from 'react';
 
 class AuthorsList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {authors: List()};
+    this.authorsPager = new ResultsPager(5, pagedInAuthors => {
+      this.setState({authors: pagedInAuthors })
+    });
+  }
+
+  componentDidMount() {
+    this.authorsPager.pageIn(this.props.authors);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.authors.equals(this.props.authors)) {
+      this.authorsPager.pageIn(this.props.authors);
+    }
+  }
+
   render() {
-    const { authors, inline } = this.props;
+    const { inline } = this.props;
+    const { authors } = this.state;
 
     const authorItems = authors.map(author => (
       <li key={author.get('id')}>
