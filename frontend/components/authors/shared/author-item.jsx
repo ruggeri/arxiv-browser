5b3bcc2e49c8653b0c9f26962cbdbbfcae09ee80
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-13 17:49:19 -0700

    bold authors who are favorited

--- frontend/components/authors/shared/author-item.jsx
+++ frontend/components/authors/shared/author-item.jsx
@@ -1,17 +1,39 @@
+import classNames from 'classnames';
 import AuthorStar from 'components/authors/shared/author-star.jsx';
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class AuthorItem extends React.Component {
   render() {
-    const { author } = this.props;
+    const { author, authorStatus } = this.props;
     const url = `/authors/${author.get('id')}`
+
+    const linkClass = classNames({
+      author: true,
+      starred: authorStatus && authorStatus.get('isStarred')
+    });
+
     return (
       <span>
-        <Link to={url}>{author.get('name')}</Link> <AuthorStar author={author}/>
+        <Link to={url} className={linkClass}>
+          {author.get('name')}
+        </Link> <AuthorStar author={author}/>
       </span>
     );
   }
 }
+
+AuthorItem = connect(
+  (state, ownProps) => {
+    const paperId = ownProps.author.get('id');
+
+    const props = {
+      authorStatus: state.authorStatuses.get(paperId),
+    };
+
+    return props;
+  },
+)(AuthorItem);
 
 export default AuthorItem;
