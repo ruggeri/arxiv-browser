656c747be800ea061cd861ad831cbbc30acfa7d9
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-15 13:42:38 -0700

    pager: perform incremental reconciliation
    Previously the pager cleared out all results to page-in new
    reuslts. This was wasteful if the new results almost entirely
    overlapped with the old results.
    
    Now, pager will build a sequence of updates to transform the old
    results to the new results. It will then perform these in batches.
    
    Even if the old and new results are very long lists, the number of
    updates required may be very small if the lists are almost identical.

--- frontend/helpers/pager.jsx
+++ frontend/helpers/pager.jsx
@@ -1,52 +1,138 @@
 import { List } from 'immutable';
 import React from 'react';
 
+// Helper classes that represent updates to a sequence.
+class RemoveUpdate {
+  constructor(index) {
+    this.index = index;
+  }
+
+  apply(list) {
+    return list.delete(this.index);
+  }
+}
+
+class InsertUpdate {
+  constructor(index, item) {
+    this.index = index;
+    this.item = item;
+  }
+
+  apply(list) {
+    return list.insert(this.index, this.item);
+  }
+}
+
+class MoveUpdate {
+  constructor(fromIndex, toIndex) {
+    this.fromIndex = fromIndex;
+    this.toIndex = toIndex
+  }
+
+  apply(list) {
+    const item = list.get(this.fromIndex);
+    return list.delete(this.fromIndex).insert(this.toIndex, item);
+  }
+}
+
+// Applies a sequence of updates to transform a list of items.
+function applyUpdateSequence(updateSequence, currentItems) {
+  updateSequence.forEach(u => {
+    if (!u.apply) {
+      debugger
+    }
+    currentItems = u.apply(currentItems)
+  });
+  return currentItems;
+}
+
+// This builds a sequence of updates to transform, one-by-one,
+// currentItems toward newItems.
+function buildUpdateSequence(currentItems, newItems) {
+  let updateSequence = List();
+
+  let currentItemsIndex = 0;
+  let newItemsIndex = 0;
+  while (currentItemsIndex < currentItems.count() || newItemsIndex < newItems.count()) {
+    const firstCurrentItem = currentItems.get(currentItemsIndex);
+    const firstNewItem = newItems.get(newItemsIndex);
+
+    if (firstCurrentItem && !newItems.includes(firstCurrentItem)) {
+      // If the current item should be removed, remove it.
+      const removeUpdate = new RemoveUpdate(currentItemsIndex);
+      updateSequence = updateSequence.push(removeUpdate);
+
+      currentItems = removeUpdate.apply(currentItems);
+    } else if (firstCurrentItem && firstCurrentItem.equals(firstNewItem)) {
+      // If the current item is in the right place, leave it.
+      currentItemsIndex += 1;
+      newItemsIndex += 1;
+    } else if (!currentItems.includes(firstNewItem)) {
+      // Current item belongs here, but needs to have a new item
+      // inserted before it.
+      const insertUpdate = new InsertUpdate(
+        currentItemsIndex,
+        firstNewItem
+      );
+      updateSequence = updateSequence.push(insertUpdate);
+
+      currentItems = insertUpdate.apply(currentItems);
+
+      currentItemsIndex += 1;
+      newItemsIndex += 1;
+    } else if (currentItems.includes(firstNewItem)) {
+      // Current item belongs here, but needs to have a subsequent
+      // item moved ahead.
+      const fromIndex = currentItems.indexOf(firstNewItem);
+      const moveUpdate = new MoveUpdate(fromIndex, currentItemsIndex);
+      updateSequence = updateSequence.push(moveUpdate);
+
+      currentItems = moveUpdate.apply(currentItems);
+
+      currentItemsIndex += 1;
+      newItemsIndex += 1;
+    } else {
+      throw "Should never be here!";
+    }
+  }
+
+  return updateSequence;
+}
+
 class PagerHelper {
-  constructor(pageSize, pagerCallback) {
+  constructor({pageSize, setCurrentItems}) {
     if (!(pageSize > 0)) {
       throw `Why is pageSize ${JSON.stringify(pageSize)}?`
     }
 
-    this.pageSize = pageSize;
-    this.pagerCallback = pagerCallback;
     this.currentWorkerId = 0;
+    this.pageSize = pageSize;
+    this.setCurrentItems = setCurrentItems;
   }
 
   delay(t) {
     return new Promise(resolve => setTimeout(resolve, t));
   }
 
-  async pageIn(resultsToPage) {
+  async pageIn(currentItems, newItems) {
     this.stopWorkers();
     const myWorkerId = this.currentWorkerId;
 
-    let pagedInResults = List();
-    // Clear out any pre-existing results.
-    this.pagerCallback(pagedInResults);
-    while (!pagedInResults.equals(resultsToPage)) {
+    // Build the update sequence once, then slowly perform these
+    // updates in batches of the pageSize.
+    let updateSequence = buildUpdateSequence(currentItems, newItems);
+    while (!updateSequence.isEmpty()) {
       if (myWorkerId != this.currentWorkerId) {
-        // someone else will take over this work.
+        // We've been pre-empted!
         return;
       }
 
-      // This effectively adds one resultsToPage every 5ms. Note that
-      // because we are using take every time, we can deal with
-      // interruptions. If we've copied N items before we are
-      // interrupted by a new call to pageIn, then the first thing
-      // the new worker does is page in the first N+1 results.
-      //
-      // The only potential problem I can see is if the user is
-      // interacting with an item, but then it gets pushed out of the
-      // first N+1 results. That is unlikely and can never happen if
-      // results are always appended, though.
-      //
-      // I'm kind of proud of this!
-      pagedInResults = resultsToPage.take(
-        pagedInResults.count() + this.pageSize
-      );
-      this.pagerCallback(pagedInResults)
+      const updateBatch = updateSequence.take(this.pageSize);
+      currentItems = applyUpdateSequence(updateBatch, currentItems);
+      this.setCurrentItems(currentItems);
+      updateSequence = updateSequence.skip(this.pageSize);
 
-      await this.delay(5);
+      await this.delay(1);
     }
   }
 
@@ -59,20 +145,23 @@ class Pager extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.pager = new PagerHelper(this.props.pageSize, pagedInResults => {
-      this.setState({items: pagedInResults});
+    this.pager = new PagerHelper({
+      pageSize: this.props.pageSize,
+      setCurrentItems: currentItems => {
+        this.setState({items: currentItems});
+      },
     });
 
     this.state = {items: List()};
   }
 
   componentDidMount() {
-    this.pager.pageIn(this.props.items);
+    this.pager.pageIn(this.state.items, this.props.items);
   }
 
   componentWillReceiveProps(nextProps) {
     if (!nextProps.items.equals(this.props.items)) {
-      this.pager.pageIn(nextProps.items);
+      this.pager.pageIn(this.state.items, nextProps.items);
     }
   }
 
