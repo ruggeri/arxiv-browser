d93fdb19448df1a3969a02a7df26a2b2df1b5ad4
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-14 17:28:46 -0700

    simplify Searcher

--- frontend/helpers/searcher.jsx
+++ frontend/helpers/searcher.jsx
@@ -1,57 +1,36 @@
 import {List} from 'immutable';
 import React from 'react';
 
-class SearchInput extends React.PureComponent {
+class Searcher extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {query: ""};
 
+    this.state = {query: ""};
     this.changeHandler = this.changeHandler.bind(this);
   }
 
   changeHandler(event) {
     this.setState({query: event.target.value});
-    this.props.changeHandler(event.target.value);
   }
 
-  render() {
-    const {query} = this.props;
-
-    return (
-      <label>
-        Search: <input type="text" value={query} onChange={this.changeHandler}/>
-      </label>
-    );
-  }
-}
-
-class Searcher extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {items: List()};
-    this.changeHandler = this.changeHandler.bind(this);
-  }
-
-  changeHandler(query) {
-    if (query === "") {
-      this.setState({items: List()});
-      return;
+  matchedItems() {
+    if (this.state.query === "") {
+      return List();
     }
 
-    const matchedItems = this.props.searcher(query);
-    this.setState({items: matchedItems});
+    return this.props.searcher(this.state.query);
   }
 
   render() {
     const {component} = this.props;
-    const {items} = this.state;
     const Component = component;
 
     return (
       <div>
-        <SearchInput changeHandler={this.changeHandler}/>
-        <Component items={items}/>
+        <label>
+          Search: <input type="text" value={this.state.query} onChange={this.changeHandler}/>
+        </label>
+        <Component items={this.matchedItems()}/>
       </div>
     );
   }
