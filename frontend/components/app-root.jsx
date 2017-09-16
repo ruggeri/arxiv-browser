c9cbce628af84b4a8d7fc52394c671cf6dd6eca2
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-15 20:40:38 -0700

    break out routes to their own component

--- frontend/components/app-root.jsx
+++ frontend/components/app-root.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Header from 'components/header.jsx';
 import PapersIndex from 'components/papers/papers-index.jsx';
 import PaperShow from 'components/papers/paper-show.jsx';
 import AuthorShow from 'components/authors/author-show.jsx';
 import AuthorsIndex from 'components/authors/authors-index.jsx';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+
+const Routes = () => (
+  <Switch>
+    <Redirect exact path="/" to="/papers"/>
+
+    <Route exact path="/authors" component={AuthorsIndex}/>
+    <Route exact path="/authors/starred" component={(props) => (
+      <AuthorsIndex filterName="starred"/>
+    )}/>
+    <Route exact path="/authors/:authorId" component={AuthorShow}/>
+    <Route exact path="/authors/starred/papers" component={(props) => (
+      <PapersIndex filterName="starredAuthors"/>
+    )}/>
+
+    <Route exact path="/papers" component={PapersIndex}/>
+    <Route exact path="/papers/starred" component={(props) => (
+      <PapersIndex filterName="starred"/>
+    )}/>
+    <Route exact path="/papers/:paperId" component={PaperShow}/>
+  </Switch>
+);
 
 const AppRoot = ({ store }) => (
   <Provider store={store}>
     <BrowserRouter>
       <div className="container">
         <Header/>
-        <Switch>
-          <Redirect exact path="/" to="/papers"/>
-
-          <Route exact path="/authors" component={AuthorsIndex}/>
-          <Route exact path="/authors/starred" component={(props) => (
-            <AuthorsIndex filterName="starred"/>
-          )}/>
-          <Route exact path="/authors/:authorId" component={AuthorShow}/>
-          <Route exact path="/authors/starred/papers" component={(props) => (
-            <PapersIndex filterName="starredAuthors"/>
-          )}/>
-
-          <Route exact path="/papers" component={PapersIndex}/>
-          <Route exact path="/papers/starred" component={(props) => (
-            <PapersIndex filterName="starred"/>
-          )}/>
-          <Route exact path="/papers/:paperId" component={PaperShow}/>
-        </Switch>
+        <Routes />
       </div>
     </BrowserRouter>
   </Provider>
--- frontend/reducers/root-reducer.js
+++ frontend/reducers/root-reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import { enableBatching } from 'redux-batched-actions';
 
-import authorsReducer from './authors-reducer';
-import authorshipsReducer from './authorships-reducer';
-import authorStatusesReducer from './author-statuses-reducer';
-import papersReducer from './papers-reducer';
-import paperStatusesReducer from './paper-statuses-reducer';
+import authorsReducer from 'reducers/authors-reducer';
+import authorshipsReducer from 'reducers/authorships-reducer';
+import authorStatusesReducer from 'reducers/author-statuses-reducer';
+import papersReducer from 'reducers/papers-reducer';
+import paperStatusesReducer from 'reducers/paper-statuses-reducer';
 
 const rootReducer = enableBatching(combineReducers({
   authors: authorsReducer,
