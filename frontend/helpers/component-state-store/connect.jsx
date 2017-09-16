3acf408cfd36eebe0eb3e1df434acaffbce0f13b
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-16 16:53:48 -0700

    add documentation for connect and StatefulComponent

--- frontend/helpers/component-state-store/connect.jsx
+++ frontend/helpers/component-state-store/connect.jsx
@@ -2,6 +2,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { StatefulComponent } from './stateful-component.jsx';
 
+// Takes a component and generates a subclass which differs in that it
+// will initialize from persistedState on mounting, persist the state on
+// state updates, and clear the state on unmounting. It then kindly
+// wraps this in a StatefulComponent so you don't have to do this.
+//
+// connect is somewhat naive and won't work in all scenarios. For
+// instance, imagine that a component state was set to show a spinner as
+// it waited for an AJAX request to be fulfilled. On navigation away
+// from the history location, the AJAX request may be canceled. But when
+// the component re-mounts on navigation back, the spinner may be shown
+// again, waiting for an AJAX response that will now never return.
 export function connect(component) {
   class ExtendedComponent extends component {
     componentWillMount() {
--- frontend/helpers/component-state-store/stateful-component.jsx
+++ frontend/helpers/component-state-store/stateful-component.jsx
@@ -1,6 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// You can use this to wrap a component and it will be passed some
+// props: clearPersistedState, getPersistedState, and persistState. Your
+// component can then use these.
+//
+// The component you wrap has to work with StatefulComponent and call
+// the appropriate prop methods at appropriate times. Therefore, there
+// can be a little shimming required to integrate with
+// StatefulComponent.
+//
+// See `connect` which aims to do this shimming for you. But `connect`
+// won't always work, so you can always fall back to StatefulComponent
+// yourself.
 export class StatefulComponent extends React.Component {
   static propTypes = {
     children: PropTypes.element.isRequired,
