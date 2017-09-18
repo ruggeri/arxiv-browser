4f57932a790d19788eb7606348bf90450c4aa108
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-18 14:17:23 -0700

    format papers results with more seperation of metadata and summary

--- frontend/components/papers/shared/paper-item.jsx
+++ frontend/components/papers/shared/paper-item.jsx
@@ -8,52 +8,68 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+const PaperTitleRow = ({ isStarred, paper }) => (
+  <div className="row title-row">
+    <div className="col-12">
+      <Link
+        to={`/papers/${paper.get('id')}`}
+        className={classNames({paper: true, starred: isStarred})}>
+        {paper.get('title')}
+      </Link>
+
+      <div className="buttons pull-right">
+        <PaperStar paper={paper}/>
+        <PaperTrash paper={paper}/>
+      </div>
+    </div>
+  </div>
+);
+
+const PaperAuthorsRow = ({ authors }) => (
+  <div className="row author-row">
+    <div className="col-11">
+      {authors && <AuthorsList authors={authors} inline={true}/>}
+    </div>
+  </div>
+);
+
+const PaperPublicationDateRow = ({ paper }) => (
+  <div className="row publication-date-row">
+    <div className="col-12">
+      Publication Date: {paper.get('publicationDateTime')}
+    </div>
+  </div>
+);
+
+const PaperMetadata = ({ paper, authors, isStarred }) => (
+  <div className="metadata-row">
+    <PaperTitleRow paper={paper} isStarred={isStarred}/>
+    <PaperAuthorsRow authors={authors}/>
+    <PaperPublicationDateRow paper={paper}/>
+  </div>
+);
+
+const PaperSummary = ({ paper }) => (
+  <div className="row summary-row">
+    <div className="col-12">
+      <p>{paper.get('summary')}</p>
+    </div>
+  </div>
+);
+
 class PaperItem extends React.PureComponent {
   render() {
     const { authors, isStarred, paper } = this.props;
-    const url = `/papers/${paper.get('id')}`
 
     const divClassName = classNames({
       "paper-item": true,
       ...this.props.classNames,
     });
 
-    const linkClass = classNames({
-      paper: true,
-      starred: isStarred,
-    });
-
-    let authorsElement = null;
-    if (authors) {
-      authorsElement = <AuthorsList authors={authors} inline={true}/>;
-    }
-
     return (
       <div className={divClassName} data-id={paper.get('id')}>
-        <div className="row title-row">
-          <div className="col-12">
-            <Link to={url} className={linkClass}>
-              {paper.get('title')}
-            </Link>
-
-            <div className="buttons pull-right">
-              <PaperStar paper={paper}/>
-              <PaperTrash paper={paper}/>
-            </div>
-          </div>
-        </div>
-
-        <div className="row author-row">
-          <div className="col-11">
-            {authorsElement}
-          </div>
-        </div>
-
-        <div className="row summary-row">
-          <div className="col-12">
-            <p>{paper.get('summary')}</p>
-          </div>
-        </div>
+        <PaperMetadata paper={paper} authors={authors} isStarred={isStarred}/>
+        <PaperSummary paper={paper}/>
       </div>
     );
   }
