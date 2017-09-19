e7fec924f4a22791e4c79a185306f2e1250418b6
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-19 00:01:31 -0700

    remove knex test file

--- test.js
+++
@@ -1,6 +0,0 @@
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/config/sequelize.json')[env];
-
-console.log(config)
-const knex = require('knex')(config);
-knex.count("* as count").from("authors").then(res => console.log(res))
