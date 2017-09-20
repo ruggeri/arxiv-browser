ff2c70a57178f57693a5fe118891eecc16239bc5
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-20 16:49:46 -0700

    update test.js to perform a reset of all stars/states

--- backend/bin/test.js
+++ backend/bin/test.js
@@ -3,35 +3,18 @@ const models = require('../models');
 
 async function main() {
   try {
-    const link = crypto.randomBytes(20).toString('hex');
-    const paper = (await (
-      models.knex
-        .insert({
-          title: "bogus",
-          link: link,
-          summary: "bogus",
-          publicationDateTime: models.knex.fn.now(),
-          updateDateTime: models.knex.fn.now(),
-          createdAt: models.knex.fn.now(),
-        })
-        .into('papers')
-        .returning('*')
-    ))[0];
-    console.log(paper)
-
-    const paperStatus = (await (
-      models.knex
-        .insert({
-          paperId: paper.id,
-          isArchived: false,
+    (await (
+      models.knex('paperStatuses')
+        .update({
+          state: 'isAwaitingReview',
           isStarred: false,
-          createdAt: models.knex.fn.now(),
         })
-        .into('paperStatuses')
-        .returning('*')
-    ))[0];
+    ));
 
-    console.log(paperStatus);
+    (await (
+      models.knex('authorStatuses')
+        .update({isStarred: false})
+    ));
   } catch (err) {
     console.log(err);
   } finally {
