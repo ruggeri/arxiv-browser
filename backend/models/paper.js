0e69dedf3ad86ff46e6666d352593a9fb22c07fa
Ned Ruggeri <ruggeri@self-loop.com>
2017-09-20 16:12:51 -0700

    perform state filters on the backend

--- backend/models/paper.js
+++ backend/models/paper.js
@@ -1,22 +1,24 @@
 const _ = require('lodash');
 
 const DEFAULT_PAPERS_LIMIT = 100;
+const PAPER_STATUS_STATES = [
+  'isAwaitingReview',
+  'isIgnored',
+  'isReviewed',
+  'isSavedForLaterReading'
+];
 
-function chainIsPaperStarred(paperQuery) {
-  return (
-    paperQuery
-      .join('paperStatuses', 'papers.id', 'paperStatuses.paperId')
-      .where('paperStatuses.isStarred', true)
+function addPaperStatusStateFilters(paperQuery, queryObj) {
+  const selectedStates = _.filter(
+    PAPER_STATUS_STATES,
+    state => queryObj[state] === "true"
   );
-}
 
-function chainIsAuthorStarred(paperQuery) {
-  return (
-    paperQuery
-      .join('authorships', 'papers.id', 'authorships.paperId')
-      .join('authorStatuses', 'authorships.authorId', 'authorStatuses.authorId')
-      .where('authorStatuses.isStarred', true)
-  );
+  if (selectedStates.length === 0) {
+    return paperQuery;
+  }
+
+  return paperQuery.whereIn('paperStatuses.state', selectedStates);
 }
 
 module.exports = (knex) => {
@@ -42,17 +44,17 @@ module.exports = (knex) => {
       );
     },
 
-    query: async (options) => {
-      const {query, limit, isPaperStarred, isAuthorStarred} = Object.assign({
+    query: async (queryObj) => {
+      const {limit, query} = Object.assign({
         limit: DEFAULT_PAPERS_LIMIT,
-      }, options);
+      }, queryObj);
 
-      const resultsByTitle = await Paper.queryByTitle(options);
+      const resultsByTitle = await Paper.queryByTitle(queryObj);
       if (!query) {
         return resultsByTitle;
       }
 
-      const resultsByAuthorName = await Paper.queryByAuthorName(options);
+      const resultsByAuthorName = await Paper.queryByAuthorName(queryObj);
       const mergedResults = (
         _(resultsByTitle)
         .unionBy(resultsByAuthorName, 'id')
@@ -64,26 +66,29 @@ module.exports = (knex) => {
       return mergedResults;
     },
 
-    queryByAuthorName: async (options) => {
+    queryByAuthorName: async (queryObj) => {
       const {query, limit, isPaperStarred, isAuthorStarred} = Object.assign({
         limit: DEFAULT_PAPERS_LIMIT,
-      }, options);
+      }, queryObj);
 
       let paperQuery = (
         knex
           .select('papers.*')
           .from('papers')
           .orderBy('publicationDateTime', 'DESC')
-          .limit(limit)
+          .join('paperStatuses', 'papers.id', 'paperStatuses.paperId')
           .join('authorships', 'papers.id', 'authorships.paperId')
           .join('authors', 'authorships.authorId', 'authors.id')
           .where('authors.name', 'ILIKE', `%${query}%`)
           .groupBy('papers.id')
+          .limit(limit)
       );
 
       if (isPaperStarred) {
-        paperQuery = chainIsPaperStarred(paperQuery);
+        paperQuery = paperQuery.where('paperStatuses.isStarred', true)
       }
+      paperQuery = addPaperStatusStateFilters(paperQuery, queryObj);
+
       if (isAuthorStarred) {
         paperQuery = paperQuery
           .join('authorStatuses', 'authors.id', 'authorStatuses.authorId')
@@ -93,26 +98,35 @@ module.exports = (knex) => {
       return await paperQuery;
     },
 
-    queryByTitle: async (options) => {
+    queryByTitle: async (queryObj) => {
       const {query, limit, isPaperStarred, isAuthorStarred} = Object.assign({
         limit: DEFAULT_PAPERS_LIMIT,
-      }, options);
+      }, queryObj);
 
       let paperQuery = (
         knex
           .select('papers.*')
           .from('papers')
-          .orderBy('publicationDateTime', 'DESC')
           .where('papers.title', 'ILIKE', `%${query}%`)
-          .limit(limit)
+          .join('paperStatuses', 'papers.id', 'paperStatuses.paperId')
           .groupBy('papers.id')
+          .orderBy('publicationDateTime', 'DESC')
+          .limit(limit)
       );
 
       if (isPaperStarred) {
-        paperQuery = chainIsPaperStarred(paperQuery);
+        paperQuery = paperQuery.where('paperStatuses.isStarred', true)
       }
+
+      paperQuery = addPaperStatusStateFilters(paperQuery, queryObj);
+
       if (isAuthorStarred) {
-        paperQuery = chainIsAuthorStarred(paperQuery);
+        paperQuery = (
+          paperQuery
+            .join('authorships', 'papers.id', 'authorships.paperId')
+            .join('authorStatuses', 'authorships.authorId', 'authorStatuses.authorId')
+            .where('authorStatuses.isStarred', true)
+        );
       }
 
       return await paperQuery;
